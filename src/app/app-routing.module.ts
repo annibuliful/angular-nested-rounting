52223f40b9bb1c6b4a371d5915662c42c9fc41e5
jarupong pajakgo <lagmanmc@gmail.com>
2020-07-07 22:47:52 +0700

    Init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-
-const routes: Routes = [];
+import { MainComponent } from './main/main.component';
+import { SubOneComponent } from './sub-one/sub-one.component';
+import { SubTwoComponent } from './sub-two/sub-two.component';
+const routes: Routes = [
+  {
+    path: '',
+    component: MainComponent,
+    children: [
+      {
+        path: 'sub-one',
+        component: SubOneComponent,
+      },
+      {
+        path: 'sub-two',
+        component: SubTwoComponent,
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { SubOneComponent } from './sub-one/sub-one.component';
+import { SubTwoComponent } from './sub-two/sub-two.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    SubOneComponent,
+    SubTwoComponent
   ],
   imports: [
     BrowserModule,
